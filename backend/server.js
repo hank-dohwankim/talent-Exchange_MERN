73e7dd081e4c88dca31d0c500ce1875cebc48e1d
hank.dohwankim <hank.dohwankim@gmail.com>
2020-10-17 23:02:07 -0500

    add backend post route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import posts from './data/posts.js';
+import postRoute from './routes/postRoutes.js';
 
 dotenv.config();
 
@@ -14,14 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/posts', (req, res) => {
-  res.json(posts);
-});
-
-app.get('/api/posts/:id', (req, res) => {
-  const post = posts.find((p) => p._id === req.params.id);
-  res.json(post);
-});
+app.use('/api/posts', postRoute);
 
 const PORT = process.env.PORT || 5000;
 
